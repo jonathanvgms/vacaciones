b81382aadb819da04ab317db5a9745931329d4cd
reneterrazas <151966341+reneterrazas@users.noreply.github.com>
2025-10-15 18:21:54 -0300

    nose

--- VacacionesApi/Program.cs
+++ VacacionesApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VacacionesApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<Aprobacion>();
 
 var app = builder.Build();
 
