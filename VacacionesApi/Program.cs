8e77e47d09bbaa6148b1c2f296607108bdcc4da7
quispeleon <151966341+reneterrazas@users.noreply.github.com>
2025-10-30 09:27:22 -0300

    modificacion en el programs

--- VacacionesApi/Program.cs
+++ VacacionesApi/Program.cs
@@ -2,16 +2,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using VacacionesApi.Services;
+using Microsoft.EntityFrameworkCore;
+using VacacionesApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 23))
+            )
+);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<Aprobacion>();
-
+builder.Services.AddScoped<AprobacionService>();
+builder.Services.AddScoped<DepartamentoService>();
+builder.Services.AddScoped<EmpleadoService>();
+builder.Services.AddScoped<FeriadoService>();
+builder.Services.AddScoped<NotificacionService>();
+builder.Services.AddScoped<RolService>();
+builder.Services.AddScoped<SaldoVacacionesService>();
+builder.Services.AddScoped<SolicitudService>();
+builder.Services.AddScoped<UsuarioService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
